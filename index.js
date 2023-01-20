dfd0cb643ac3c11d2d594bea9299dfcc54799450
Ulya-Kuzmina <119502689+Ulya-Kuzmina@users.noreply.github.com>
2023-01-20 19:14:27 +0300

    Update index.js
    add script

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.log('1.	Вёрстка валидная +10\n2.Вёрстка семантическая +20. Все элементы присутствуют.\n3.Вёрстка соответствует макету +48\n
-4.Требуемые  css элементы есть + 12\n5.	Интерактивность, реализуемая через css есть +20\n Итоговая оценка с учетом погрешности 100 баллов.
-')
+var cityChoice = document.querySelector('.contact_choice div');
+var cityOptions = document.querySelector('.contact_choice ul');
+var cityChoiceArrow = document.querySelector('.contact_choice_arrow');
+
+cityChoice.onclick = () => {
+    if (cityChoice.classList.contains('menu')) {
+        cityOptions.style.display = "none";
+    } else {
+        cityOptions.style.display = "block";
+    }
+    cityChoice.classList.toggle('menu')
+    cityChoiceArrow.classList.toggle('contact_choice_arrow_active')
+}
