19e8b4abbe41d40d86bdc932e8d385930d424b91
Ulya-Kuzmina <119502689+Ulya-Kuzmina@users.noreply.github.com>
2023-02-05 16:27:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-var cityChoice = document.querySelector('.contact_choice div');
-var cityOptions = document.querySelector('.contact_choice ul');
-var cityChoiceArrow = document.querySelector('.contact_choice_arrow');
+const cityChoice = document.querySelector('.contact_choice div');
+const cityOptionsBlock = document.querySelector('.contact_choice ul');
+const cityChoiceArrow = document.querySelector('.contact_choice_arrow');
 
-cityChoice.onclick = () => {
+const cityChoiceBlockLogic = () => {
     if (cityChoice.classList.contains('menu')) {
-        cityOptions.style.display = "none";
+        cityOptionsBlock.style.display = "none";
     } else {
-        cityOptions.style.display = "block";
+        cityOptionsBlock.style.display = "block";
     }
     cityChoice.classList.toggle('menu')
     cityChoiceArrow.classList.toggle('contact_choice_arrow_active')
 }
+
+cityChoice.onclick = cityChoiceBlockLogic
+
+const cityAdresses = document.querySelectorAll('.contact_choice li');
+const city = document.querySelector('.contact_choice span');
+const contacts = document.querySelector('.contact_choice_city');
+const contactCity = contacts.querySelector('.contact_choice_city_city');
+const contactPhone = contacts.querySelector('.contact_choice_city_phone');
+const contactAdress = contacts.querySelector('.contact_choice_city_adress');
+const contactsMap = {
+    'Canandaigua, NY': {
+        city: 'Canandaigua, NY', 
+        phone: '+1 585 393 0001',
+        address: '151 Charlotte Street'
+    },
+    'New York City': {
+        city: 'New York City', 
+        phone: '+1	212	456 0002',
+        address: '9 East 91st Street'
+    },
+    'Yonkers, NY': {
+        city: 'Yonkers, NY', 
+        phone: '+1	914	678 0003',
+        address: '511 Warburton Ave'
+    },
+    'Sherrill, NY': {
+        city: 'Sherrill, NY', 
+        phone: '+1	315	908 0004',
+        address: '14 WEST Noyes BLVD'
+    }
+};
+
+cityAdresses.forEach(cityAdress => {
+    cityAdress.onclick = () => {
+        city.innerText = cityAdress.innerText;
+        cityChoiceBlockLogic();
+        contactCity.innerText = contactsMap[city.innerText].city;
+        contactPhone.innerText = contactsMap[city.innerText].phone;
+        contactAdress.innerText = contactsMap[city.innerText].address;
+        contacts.classList.add('contact_choice_city_visible')
+    }
+});
